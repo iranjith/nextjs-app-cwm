a3332dbfc9aaf2aff6496cdd579fccae8ee41b6f
Ranjith.Kumar <theranjith@gmail.com>
2024-07-08 18:45:32 +0530

    access query string params

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,37 +1,46 @@
-import React from 'react'
+import { sort } from "fast-sort";
+import Link from "next/link";
+import React from "react";
 
 interface User {
-    id: number;
-    name: string;
-    email: string;
-  }
-
-const UserTable = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-        cache: "no-store",
-        next: {
-          revalidate: 10,
-        },
-      });
-      const users: User[] = await res.json();
-  return (
-    <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>E-mail</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-  )
+  id: number;
+  name: string;
+  email: string;
 }
 
-export default UserTable
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (user) => user.email : (user) => user.name
+  );
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedUsers.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import UserTable from "./UserTable";
 
+interface Props {
+  searchParams: { sortOrder: string };
+}
 
-
-const UsersPage = async () => {
- 
-
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
-      <UserTable />
+      <UserTable sortOrder={sortOrder} />
     </>
   );
 };

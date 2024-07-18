d1dd4700d04208612183bb50ce1dbd41be42556d
Ranjith.Kumar <theranjith@gmail.com>
2024-07-18 07:12:28 +0530

    update users data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -19,7 +19,7 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   //validate the request body
   const body = await request.json();
@@ -28,9 +28,26 @@ export async function PUT(
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-  return NextResponse.json({ id: 1, name: body.name });
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export function DELETE(
